d288ba294c07b071ecd6460e3d243c6e7c8d3f54
SnigdhaReddy09 <93863995+SnigdhaReddy09@users.noreply.github.com>
2024-10-25 12:06:41 +0530

    Add files via upload

---
+++ for of.js
@@ -0,0 +1,37 @@
+// 1
+var day="monday"
+switch (day) {
+    case "sunday":
+        console.log("1st day of week")
+        break;
+    case "monday":
+        console.log("2nd day of week")
+        break;
+    case "tuesday":
+        console.log("3rd day of week")
+        break;
+    case "wednesday":
+        console.log("4th day of week")
+        break;
+    case "thursday":
+        console.log("5th day of week")
+        break;
+    case "friday":
+        console.log("6th day of week")
+        break;
+    case "saturday":
+        console.log("7th day of week")
+        break;
+    default:
+        console.log("invalid")    
+}
+
+// 2
+var a=[1,2,3,4,5]
+for(var elements of a)
+    console.log(elements)
+
+// 3
+var b="snigdha"
+for(var char of b)
+    console.log(char)
